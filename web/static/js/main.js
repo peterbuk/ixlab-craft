b29b95bb884dcd071b04dbe6345738b7797559e0
peterbuk <peterbuk8@gmail.com>
2020-03-24 18:17:01 -0700

    changing lightbox

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -17,3 +17,37 @@ function toggleThemeBtn(t) {
         t.innerHTML = "See More";
     }
 }
+
+$(document).ready(function() {
+    $('.featured-image').magnificPopup({
+		type: 'image',
+        tLoading: 'Loading image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile mfp-with-zoom',
+		image: {
+			tError: '<a href="%url%">The image could not be loaded.</a>',
+        },
+        zoom: {
+            enabled: true,
+            easing: 'ease-in-out',
+            duration: 200
+        },
+	});
+
+
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        },
+        
+	});
+});
